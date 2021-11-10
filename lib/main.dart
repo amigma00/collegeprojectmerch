4166c15c35d2bfb3f7d58384bd4ecc4b1518bf43
hrithikm2 <h.mishra2feb01@gmail.com>
2021-11-11 01:32:47 +0530

    Google login done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:collegeprojectmerch/view/editprofile.dart';
-import 'package:collegeprojectmerch/view/hospital.dart';
 import 'package:collegeprojectmerch/view/selection.dart';
-import 'package:collegeprojectmerch/view/shop.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() {
+import 'model/signinmodel.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,14 +18,65 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => GoogleSignInProvider(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
-      home: Hospital()
+      home: MyHomePage(),
+    ),
+  );
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("Something went wrong!"),
+            );
+          } else if (snapshot.hasData) {
+            return Selection();
+          } else
+            return Center(child: SignupWidget());
+        });
+  }
+}
+class SignupWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  final provider =
+                  Provider.of<GoogleSignInProvider>(context, listen: false);
+                  provider.googlelogin();
+                },
+                label: Text("Sign in With Google"),
+                icon: FaIcon(FontAwesomeIcons.google),
+              ),
+            )
+          ])),
     );
   }
 }
